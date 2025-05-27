4f31d8ef24d6613e2526f81fe8f020e5db59dbb9
John Paul <johpaul.vinluan2004@gmail.com>
2025-05-27 23:07:56 +0800

    Building basic widgets for the login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
+import 'package:chat_app/login_page.dart';
 import 'package:flutter/material.dart';
-import 'chat_page.dart';
-//import 'login_page.dart';
 
 void main() {
   runApp(ChatApp());
@@ -9,31 +8,10 @@ void main() {
 class ChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
-      title: 'Chat App',
-      theme: ThemeData(
-        primarySwatch: Colors.yellow,
-      ),
+      title: "Chat App",
+      theme: ThemeData(primarySwatch: Colors.yellow),
       home: LoginPage(),
-    );
-  }
-}
-
-class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      drawer: Drawer(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print('Button clicked');
-        },
-      ),
-      body: Text('Hello'),
     );
   }
 }
