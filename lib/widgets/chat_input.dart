83127a7c17339ceb6ecfad3260e91ea2bfe1a96a
John Paul <johpaul.vinluan2004@gmail.com>
2025-05-28 00:31:02 +0800

    Creating your own custom widget

--- lib/widgets/chat_input.dart
+++ lib/widgets/chat_input.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 class ChatInput extends StatelessWidget {
-  const ChatInput({super.key});
+  const ChatInput({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 80,
+      height: 100,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
