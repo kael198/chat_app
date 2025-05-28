b96cfcbf815d9a58c165a55c3883d7cf4f020d52
John Paul <johpaul.vinluan2004@gmail.com>
2025-05-28 17:53:21 +0800

    Modeling entities in Dart

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/models/chat_message_entity.dart';
 import 'package:chat_app/widgets/chat_bubble.dart';
 import 'package:chat_app/widgets/chat_input.dart';
 import 'package:flutter/material.dart';
@@ -17,7 +18,7 @@ class ChatPage extends StatelessWidget {
         actions: [
           IconButton(
               onPressed: () {
-                Navigator.pop(context);
+                Navigator.pushReplacementNamed(context, '/');
                 print('Icon pressed!');
               },
               icon: Icon(Icons.logout))
@@ -26,7 +27,6 @@ class ChatPage extends StatelessWidget {
       body: Column(
         children: [
           Expanded(
-              //TODO: Create a dynamic sized list
               child: ListView.builder(
                   itemCount: 10,
                   itemBuilder: (context, index) {
@@ -34,7 +34,12 @@ class ChatPage extends StatelessWidget {
                         alignment: index % 2 == 0
                             ? Alignment.centerLeft
                             : Alignment.centerRight,
-                        message: "Hello, this is Pooja!");
+                        //TODO: Add more chat & author related properties here
+                        entity: ChatMessageEntity(
+                            id: '1234',
+                            text: 'Hello this is Pooja!!!!!',
+                            createdAt: DateTime.now().millisecondsSinceEpoch,
+                            author: Author(userName: 'poojab26')));
                   })),
           ChatInput(),
         ],
