dbd073e3e95c4f66076777ad3a61537d1094040c
John Paul <johpaul.vinluan2004@gmail.com>
2025-05-29 08:37:48 +0800

    Creating rounded UI elements

--- lib/widgets/chat_bubble.dart
+++ lib/widgets/chat_bubble.dart
@@ -10,12 +10,13 @@ class ChatBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool isAuthor = entity.author.userName == "poojab26";
+
     return Align(
       alignment: alignment,
       child: Container(
         constraints:
-            BoxConstraints(maxWidth: MediaQuery.of(context).size.width * 0.5),
-        //TODO: Fix chat bubble width
+            BoxConstraints(maxWidth: MediaQuery.of(context).size.width * 0.6),
         padding: EdgeInsets.all(24),
         child: Column(
           mainAxisSize: MainAxisSize.min,
@@ -25,15 +26,19 @@ class ChatBubble extends StatelessWidget {
               style: TextStyle(fontSize: 20, color: Colors.white),
             ),
             if (entity.imageUrl != null)
-              Image.network(
-                '${entity.imageUrl}',
+              Container(
                 height: 200,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    image:
+                        DecorationImage(image: NetworkImage(entity.imageUrl!)),
+                    borderRadius: BorderRadius.circular(12)),
               )
           ],
         ),
-        margin: EdgeInsets.all(50),
+        margin: EdgeInsets.all(10),
         decoration: BoxDecoration(
-            color: Colors.grey,
+            color: isAuthor ? Theme.of(context).primaryColor : Colors.black87,
             borderRadius: BorderRadius.only(
                 topLeft: Radius.circular(12),
                 topRight: Radius.circular(12),
